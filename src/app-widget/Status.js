d2ef788ae15a3339be2b65d7c6b9510d6bdbad51
XiaomaiTX <me@xiaomaitx.com>
2026-01-20 03:41:33 +0800

    feat(app-widget): add Status widget with layout and data display for GitHub user statistics This commit introduces a new Status widget that displays GitHub user statistics such as total stars earned, commits, pull requests, issues, and contributions. The widget includes a layout file for positioning UI elements and fetches data from local storage to present a comprehensive overview of the user's GitHub activity.

---
+++ src/app-widget/Status.js
@@ -0,0 +1,186 @@
+import * as hmUI from "@zos/ui";
+import { px } from "@zos/utils";
+import * as hmRouter from "@zos/router";
+import { LocalStorage } from "@zos/storage";
+
+import { BasePage } from "@zeppos/zml/base-page";
+import * as Layout from "./Status.layout";
+
+AppWidget(
+    BasePage({
+        onInit() {
+            console.log("Status widget initialized");
+        },
+
+        onDataRestore() {},
+
+        build() {
+            hmUI.setAppWidgetSize({
+                h: Layout.WIDGET_HEIGHT,
+            });
+
+            try {
+                const backgroundFillRect = hmUI.createWidget(
+                    hmUI.widget.FILL_RECT,
+                    {
+                        ...Layout.BACKGROUND_FILL_RECT,
+                        color: 0x0d1117,
+                        radius: px(36),
+                    },
+                );
+                const backgroundStrokeRect = hmUI.createWidget(
+                    hmUI.widget.STROKE_RECT,
+                    {
+                        ...Layout.BACKGROUND_STROKE_RECT,
+                        radius: px(36),
+                        line_width: 2,
+                        color: 0x30363d,
+                    },
+                );
+
+                const localStorage = new LocalStorage();
+                const userStatusData = JSON.parse(
+                    localStorage.getItem("github-widget.userStatus"),
+                );
+
+                const totalStarEarned = () => {
+                    let totalStarEarn = 0;
+                    for (const repo of userStatusData.data.user.repositories
+                        .nodes) {
+                        totalStarEarn += repo.stargazerCount;
+                    }
+                    return totalStarEarn;
+                };
+                const totalCommits = () => {
+                    return (
+                        userStatusData.data.user.currentYearContributions
+                            .totalCommitContributions || "unknown"
+                    );
+                };
+                const totalPRs = () => {
+                    return (
+                        userStatusData.data.user.currentYearContributions
+                            .totalPullRequestContributions || "unknown"
+                    );
+                };
+                const totalIssues = () => {
+                    return (
+                        userStatusData.data.user.currentYearContributions
+                            .totalIssueContributions || "unknown"
+                    );
+                };
+                const totalContributions = () => {
+                    return (
+                        userStatusData.data.user.lastYearContributions
+                            .totalRepositoriesWithContributedCommits ||
+                        "unknown"
+                    );
+                };
+
+                const titleText = hmUI.createWidget(hmUI.widget.TEXT, {
+                    ...Layout.TITLE_TEXT,
+                    text_size: px(26),
+                    color: 0x57a5fd,
+                    text: `${userStatusData.data.user.login || "unknown"}'s Status`,
+                });
+
+                const totalStarEarnedIcon = hmUI.createWidget(hmUI.widget.IMG, {
+                    ...Layout.TOTAL_STAR_EARNED_ICON,
+                    src: "Status/total-star-earned-icon.png",
+                });
+                const totalStarEarnedText = hmUI.createWidget(
+                    hmUI.widget.TEXT,
+                    {
+                        ...Layout.TOTAL_STAR_EARNED_TEXT,
+                        text_size: px(22),
+                        color: 0xc3d1d9,
+                        text: `Total Star Earned:`,
+                    },
+                );
+                const totalStarEarnedValue = hmUI.createWidget(hmUI.widget.TEXT, {
+                    ...Layout.TOTAL_STAR_EARNED_VALUE,
+                    text_size: px(22),
+                    color: 0xc3d1d9,
+                    text: `${totalStarEarned()}`,
+                });
+
+                const totalCommitsIcon = hmUI.createWidget(hmUI.widget.IMG, {
+                    ...Layout.TOTAL_COMMITS_ICON,
+                    src: "Status/total-commits-icon.png",
+                });
+                const totalCommitsText = hmUI.createWidget(hmUI.widget.TEXT, {
+                    ...Layout.TOTAL_COMMITS_TEXT,
+                    text_size: px(22),
+                    color: 0xc3d1d9,
+                    text: `Total Commits:`,
+                });
+                const totalCommitValue = hmUI.createWidget(hmUI.widget.TEXT, {
+                    ...Layout.TOTAL_COMMITS_VALUE,
+                    text_size: px(22),
+                    color: 0xc3d1d9,
+                    text: `${totalCommits()}`,
+                });
+                
+                const totalPRsIcon = hmUI.createWidget(hmUI.widget.IMG, {
+                    ...Layout.TOTAL_PRS_ICON,
+                    src: "Status/total-prs-icon.png",
+                });
+                const totalPRsText = hmUI.createWidget(hmUI.widget.TEXT, {
+                    ...Layout.TOTAL_PRS_TEXT,
+                    text_size: px(22),
+                    color: 0xc3d1d9,
+                    text: `Total PRs:`,
+                });
+                const totalPRsValue = hmUI.createWidget(hmUI.widget.TEXT, {
+                    ...Layout.TOTAL_PRS_VALUE,
+                    text_size: px(22),
+                    color: 0xc3d1d9,
+                    text: `${totalPRs()}`,
+                });
+                const totalIssuesIcon = hmUI.createWidget(hmUI.widget.IMG, {
+                    ...Layout.TOTAL_ISSUES_ICON,
+                    src: "Status/total-issues-icon.png",
+                });
+                const totalIssuesText = hmUI.createWidget(hmUI.widget.TEXT, {
+                    ...Layout.TOTAL_ISSUES_TEXT,
+                    text_size: px(22),
+                    color: 0xc3d1d9,
+                    text: `Total Issues:`,
+                });
+                const totalIssuesValue = hmUI.createWidget(hmUI.widget.TEXT, {
+                    ...Layout.TOTAL_ISSUES_VALUE,
+                    text_size: px(22),
+                    color: 0xc3d1d9,
+                    text: `${totalIssues()}`,
+                });
+                const totalContributionsIcon = hmUI.createWidget(
+                    hmUI.widget.IMG,
+                    {
+                        ...Layout.TOTAL_CONTRIBUTIONS_ICON,
+                        src: "Status/total-contributions-icon.png",
+                    },
+                );
+                const totalContributionsText = hmUI.createWidget(
+                    hmUI.widget.TEXT,
+                    {
+                        ...Layout.TOTAL_CONTRIBUTIONS_TEXT,
+                        text_size: px(22),
+                        color: 0xc3d1d9,
+                        text: `Last Year Contributions:`,
+                    },
+                );
+                const totalContributionsValue = hmUI.createWidget(hmUI.widget.TEXT, {
+                    ...Layout.TOTAL_CONTRIBUTIONS_VALUE,
+                    text_size: px(22),
+                    color: 0xc3d1d9,
+                    text: `${totalContributions()}`,
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        onResume() {
+        },
+    }),
+);
---
+++ src/app-widget/Status.layout.js
@@ -0,0 +1,127 @@
+export const WIDGET_HEIGHT = px(250);
+
+export const BACKGROUND_FILL_RECT = {
+    x: px(40),
+    y: px(0),
+    w: px(400),
+    h: px(250),
+};
+
+export const BACKGROUND_STROKE_RECT = {
+    x: px(40),
+    y: px(0),
+    w: px(400),
+    h: px(250),
+};
+
+export const TITLE_TEXT = {
+    x: px(40 + 22),
+    y: px(15),
+    w: px(352),
+    h: px(44),
+};
+
+export const TOTAL_STAR_EARNED_ICON = {
+    x: px(40 + 22 + 0),
+    y: px(58 + 0 + 3),
+    w: px(24),
+    h: px(24),
+};
+
+export const TOTAL_STAR_EARNED_TEXT = {
+    x: px(40 + 22 + 35),
+    y: px(58 + 0),
+    w: px(352),
+    h: px(33),
+};
+
+export const TOTAL_STAR_EARNED_VALUE = {
+    x: px(40 + 22 + 310),
+    y: px(58 + 0),
+    w: px(352),
+    h: px(33),
+};
+
+export const TOTAL_COMMITS_ICON = {
+    x: px(40 + 22 + 0),
+    y: px(58 + 35 + 3),
+    w: px(24),
+    h: px(24),
+};
+
+export const TOTAL_COMMITS_TEXT = {
+    x: px(40 + 22 + 35),
+    y: px(58 + 35),
+    w: px(352),
+    h: px(33),
+};
+
+export const TOTAL_COMMITS_VALUE = {
+    x: px(40 + 22 + 310),
+    y: px(58 + 35),
+    w: px(352),
+    h: px(33),
+};
+
+export const TOTAL_PRS_ICON = {
+    x: px(40 + 22 + 0),
+    y: px(58 + 70 + 3),
+    w: px(24),
+    h: px(24),
+};
+
+export const TOTAL_PRS_TEXT = {
+    x: px(40 + 22 + 35),
+    y: px(58 + 70),
+    w: px(352),
+    h: px(33),
+};
+
+export const TOTAL_PRS_VALUE = {
+    x: px(40 + 22 + 310),
+    y: px(58 + 70),
+    w: px(352),
+    h: px(33),
+};
+
+export const TOTAL_ISSUES_ICON = {
+    x: px(40 + 22 + 0),
+    y: px(58 + 105 + 3),
+    w: px(24),
+    h: px(24),
+};
+
+export const TOTAL_ISSUES_TEXT = {
+    x: px(40 + 22 + 35),
+    y: px(58 + 105),
+    w: px(352),
+    h: px(33),
+};
+
+export const TOTAL_ISSUES_VALUE = {
+    x: px(40 + 22 + 310),
+    y: px(58 + 105),
+    w: px(352),
+    h: px(33),
+};
+
+export const TOTAL_CONTRIBUTIONS_ICON = {
+    x: px(40 + 22 + 0),
+    y: px(58 + 140 + 3),
+    w: px(24),
+    h: px(24),
+};
+
+export const TOTAL_CONTRIBUTIONS_TEXT = {
+    x: px(40 + 22 + 35),
+    y: px(58 + 140),
+    w: px(352),
+    h: px(33),
+};
+
+export const TOTAL_CONTRIBUTIONS_VALUE = {
+    x: px(40 + 22 + 310),
+    y: px(58 + 140),
+    w: px(352),
+    h: px(33),
+};
